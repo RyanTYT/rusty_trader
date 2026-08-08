a12a5366819c9575d03c9070fd6c45f7098ec3c2
Ryan Tan <rtytmain@gmail.com>
2026-08-08 16:01:13 +0800

    deleted trading-app-old legacy python code for python trading-program (no longer relevant)

--- trading-app-old/typings/apscheduler/__init__.pyi
+++
@@ -1,6 +0,0 @@
-from _typeshed import Incomplete
-
-release: Incomplete
-version_info: Incomplete
-version: Incomplete
-__version__: Incomplete
--- trading-app-old/typings/apscheduler/triggers/base.pyi
+++
@@ -1,6 +0,0 @@
-from abc import ABCMeta, abstractmethod
-
-
-class BaseTrigger(metaclass=ABCMeta):
-    @abstractmethod
-    def get_next_fire_time(self, previous_fire_time, now): ...  # type: ignore
